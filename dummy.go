736eba68a1dd1cb792c453a039682cf4d2b0fa96
iiiusi0n <iiiusi0n@wyvern.pw>
2023-08-07 00:07:55 +0900

    Make fit information more readable

--- dummy.go
+++ dummy.go
@@ -11,16 +11,13 @@ var (
 		"Condor",
 	}
 
-	fakeMsg = "Loss Count: 7\n" +
-		"<Prop Analysis>\n" +
-		"AB/MWD/DUAL PROP/NO PROP: 5/1/0/2\n" +
-		"Prop uses: MWD AB AB AB AB AB NP NP\n" +
-		"\n" +
-		"<Weapon Analysis>\n" +
-		"Long Range/Short Range: 2/6\n" +
-		"Weapon uses: LR LR SR SR SR SR SR SR\n" +
-		"\n" +
-		"<Web/Scram Analysis>\n" +
-		"Web/Scram: 8/8\n" +
-		"EWAR uses: WS WS WS WS WS WS WWS WWS\n"
+	fakeDetail = [][]string{
+		{"Date", "Prop", "Scram", "Point", "Web", "Neut", "Damp"},
+		{"1 day ago", "1mn AB", "O", "X", "1", "X", "X"},
+		{"5 days ago", "1mn AB", "O", "X", "1", "X", "X"},
+		{"23 days ago", "1mn AB", "O", "X", "1", "X", "X"},
+		{"2023-01-15", "X", "O", "X", "2", "X", "X"},
+		{"2023-01-14", "5mn MWD", "X", "O", "X", "X", "X"},
+		{"2023-01-02", "1mn AB", "X", "X", "2", "X", "X"},
+	}
 )
--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+	"image/color"
+	"strconv"
 )
 
 func main() {
@@ -29,17 +32,17 @@ func main() {
 	playerEntry := createPlayerEntry(playerName)
 	searchButton := createSearchButton(playerName)
 	clipboardWatcherSwitch := createClipboardWatcherSwitch(playerEntry, searchButton, clipboardWatcher)
-	resultList, detailLabel := createResultWidgets()
+	resultList, detailInfo := createResultWidgets()
 
 	// Create sub-container for player entry, search button, and clipboard watcher switch.
 	subContainer := createInputContainer(playerEntry, searchButton, clipboardWatcherSwitch)
 
 	// Create the main container with a horizontal split for result list and detail label.
-	mainContainer := createMainContainer(subContainer, resultList, detailLabel)
+	mainContainer := createMainContainer(subContainer, resultList, detailInfo)
 
 	// Set the main container as the content of the window, resize it, and show the window.
 	w.SetContent(mainContainer)
-	w.Resize(fyne.NewSize(800, 600))
+	w.Resize(fyne.NewSize(800, 400))
 	w.ShowAndRun()
 }
 
@@ -84,25 +87,51 @@ func createClipboardWatcherSwitch(playerEntry *widget.Entry, searchButton *widge
 }
 
 // createResultWidgets creates widgets for the result list and detail label.
-func createResultWidgets() (*widget.List, *widget.Label) {
+func createResultWidgets() (*widget.List, *widget.Table) {
 	resultList := widget.NewList(
 		func() int {
 			return len(ships)
 		},
 		func() fyne.CanvasObject {
-			return widget.NewLabel("Ships")
+			return widget.NewLabel("template")
 		},
 		func(id widget.ListItemID, item fyne.CanvasObject) {
 			item.(*widget.Label).SetText(ships[id])
 		},
 	)
 
-	detailLabel := widget.NewLabel("")
-	resultList.OnSelected = func(id widget.ListItemID) {
-		detailLabel.SetText(fakeMsg)
+	detailInfo := widget.NewTable(
+		func() (int, int) {
+			return len(fakeDetail), len(fakeDetail[0])
+		},
+		func() fyne.CanvasObject {
+			return canvas.NewText("template", color.White)
+		},
+		func(i widget.TableCellID, o fyne.CanvasObject) {
+			o.(*canvas.Text).Text = fakeDetail[i.Row][i.Col]
+			o.(*canvas.Text).Alignment = fyne.TextAlignCenter
+			if i.Row == 0 {
+				o.(*canvas.Text).TextStyle = fyne.TextStyle{Bold: true}
+			} else {
+				if fakeDetail[i.Row][i.Col] == "O" {
+					o.(*canvas.Text).Color = color.RGBA{R: 255, G: 0, B: 0, A: 255}
+				} else if fakeDetail[i.Row][i.Col] == "X" {
+					o.(*canvas.Text).Color = color.RGBA{R: 0, G: 255, B: 0, A: 255}
+				} else if _, err := strconv.Atoi(fakeDetail[i.Row][i.Col]); err == nil {
+					o.(*canvas.Text).Color = color.RGBA{R: 255, G: 255, B: 0, A: 255}
+				}
+			}
+		},
+	)
+
+	detailInfo.SetColumnWidth(0, 100)
+	detailInfo.SetColumnWidth(1, 100)
+
+	for i := 0; i < len(fakeDetail); i++ {
+		detailInfo.SetRowHeight(i, 30)
 	}
 
-	return resultList, detailLabel
+	return resultList, detailInfo
 }
 
 // createInputContainer creates a container for player entry, search button, and clipboard watcher switch.
@@ -116,8 +145,8 @@ func createInputContainer(playerEntry *widget.Entry, searchButton *widget.Button
 }
 
 // createMainContainer creates the main container with a horizontal split for result list and detail label.
-func createMainContainer(subContainer *fyne.Container, resultList *widget.List, detailLabel *widget.Label) *fyne.Container {
-	resultContainer := container.NewHSplit(resultList, detailLabel)
+func createMainContainer(subContainer *fyne.Container, resultList *widget.List, detailInfo *widget.Table) *fyne.Container {
+	resultContainer := container.NewHSplit(resultList, detailInfo)
 	resultContainer.SetOffset(0.3)
 
 	return container.New(layout.NewBorderLayout(subContainer, nil, nil, nil), subContainer, resultContainer)
